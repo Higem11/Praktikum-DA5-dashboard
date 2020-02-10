6665fe59835d5389f627723bb5f60cbf364f3f84
Higem11 <58914433+Higem11@users.noreply.github.com>
2020-02-10 22:21:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -137,15 +137,13 @@ app.layout = html.Div(
         ),
         # progress bar
 	html.Div([
-		  html.H6('Прогресс прохождения курса'), html.Div([
-		  						dcc.Interval(id="progress-interval", n_intervals=0, interval=500),
-		  						dbc.Progress(id="progress", color="success", style={"height": "30px"}, 
-									     className='progress_bar')
-		  						 ])
-		], 
-		id="headerr",
-            	className="row flex-display"
-		),
+		  html.H6('Прогресс прохождения курса'), 
+		  html.Div([
+		  	dcc.Interval(id="progress-interval", n_intervals=0, interval=500),
+			dbc.Progress(id="progress", color="success", style={"height": "30px"}, 
+				     className='progress_bar')
+		  	  ])
+		]),
 	# metrics
         html.Div(
             [
