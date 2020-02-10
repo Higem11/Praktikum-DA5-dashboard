10ba73fdedc226d92d17b92e2c2171219663e6b8
Higem11 <58914433+Higem11@users.noreply.github.com>
2020-02-10 21:04:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ from dash.dependencies import Input, Output
 
 import dash_core_components as dcc
 
+import dash_bootstrap_components as dbc
+
 import dash_html_components as html
 
 import plotly.graph_objs as go
