71c415fe9e56a46ba2aed8ae5b09335fb1eefa28
Higem11 <58914433+Higem11@users.noreply.github.com>
2020-02-11 13:32:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -84,38 +84,6 @@ tab_selected_style = {
 
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__)
-app.index_string = """<!DOCTYPE html>
-		<html>
-			<head>
-			<!— Yandex.Metrika counter —>
-			<script type="text/javascript" >
-			(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-			m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-			(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-			ym(57472615, "init", {
-			clickmap:true,
-			trackLinks:true,
-			accurateTrackBounce:true,
-			webvisor:true
-			});
-			</script>
-			<noscript><div><img src="https://mc.yandex.ru/watch/57472615" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-			<!— /Yandex.Metrika counter —>
-			{%metas%}
-			<title>{%title%}</title>
-			{%favicon%}
-			{%css%}
-			</head>
-			<body>
-			{%app_entry%}
-			<footer>
-			{%config%}
-			{%scripts%}
-			{%renderer%}
-			</footer>
-			</body>
-		</html>"""
 server = app.server
 #---------------------/CSS + app---------------------------------- 
 
